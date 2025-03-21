0ab5bd70673efe0a9febfc903c60684f2cdf8f3e
mjFallah <mjfallah73@gmail.com>
2025-03-21 16:06:56 +0330

    add toolbar component

--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -1,11 +1,11 @@
-interface WorkspacePageProps {
-  params: {
-    workspaceId: string;
-  };
-}
+"use client";
+import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
+import { useWorkspaceId } from "@/hooks/use-workspaceId";
 
-const WorkspacePage = ({ params }: WorkspacePageProps) => {
-  return <div>{params.workspaceId}</div>;
+const WorkspacePage = () => {
+  const workspaceId = useWorkspaceId();
+  const { data } = useGetWorkspace({ id: workspaceId });
+  return <div>{JSON.stringify(data)}</div>;
 };
 
 export default WorkspacePage;
