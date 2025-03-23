6dbf88acfa7e71905d9ad33830688ef7e0de798c
mjFallah <mjfallah73@gmail.com>
2025-03-23 17:44:30 +0330

    add invite modal and regenerate join code functionality

--- src/app/workspace/[workspaceId]/workspace-header.tsx
+++ src/app/workspace/[workspaceId]/workspace-header.tsx
@@ -11,6 +11,7 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { Hint } from "@/components/hint";
 
+import { InviteModal } from "./invite-modal";
 import { PreferencesModal } from "./preferences-modal";
 
 import { Doc } from "../../../../convex/_generated/dataModel";
@@ -24,9 +25,16 @@ export const WorkspaceHeader = ({
   workspace,
   isAdmin,
 }: WorkspaceHeaderProps) => {
+  const [inviteOpen, setInviteOpen] = useState(false);
   const [preferencesOpen, setPreferencesOpen] = useState(false);
   return (
     <>
+      <InviteModal
+        open={inviteOpen}
+        setOpen={setInviteOpen}
+        name={workspace.name}
+        joinCode={workspace.joinCode}
+      />
       <PreferencesModal
         open={preferencesOpen}
         setOpen={setPreferencesOpen}
@@ -61,7 +69,7 @@ export const WorkspaceHeader = ({
               <>
                 <DropdownMenuItem
                   className="cursor-pointer py-2"
-                  onClick={() => {}}
+                  onClick={() => setInviteOpen(true)}
                 >
                   Invite people to {workspace.name}
                 </DropdownMenuItem>
