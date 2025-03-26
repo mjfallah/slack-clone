e1cd24facef1d758621f39102ff1d56720ea11c8
mjFallah <mjfallah73@gmail.com>
2025-03-26 22:12:04 +0330

    add edit and delete message functionality

--- src/components/toolbar.tsx
+++ src/components/toolbar.tsx
@@ -36,7 +36,12 @@ export const Toolbar = ({
         </EmojiPopover>
         {!hideThreadButton && (
           <Hint label="Reply in thread">
-            <Button variant="ghost" size="iconSm" disabled={isPending}>
+            <Button
+              onClick={handleThread}
+              variant="ghost"
+              size="iconSm"
+              disabled={isPending}
+            >
               <MessageSquareTextIcon className="size-4" />
             </Button>
           </Hint>
@@ -44,12 +49,22 @@ export const Toolbar = ({
         {isAuthor && (
           <>
             <Hint label="Edit message">
-              <Button variant="ghost" size="iconSm" disabled={isPending}>
+              <Button
+                onClick={handleEdit}
+                variant="ghost"
+                size="iconSm"
+                disabled={isPending}
+              >
                 <Pencil className="size-4" />
               </Button>
             </Hint>
             <Hint label="Delete message">
-              <Button variant="ghost" size="iconSm" disabled={isPending}>
+              <Button
+                onClick={handleDelete}
+                variant="ghost"
+                size="iconSm"
+                disabled={isPending}
+              >
                 <Trash className="size-4" />
               </Button>
             </Hint>
